b709b5a4666a83f8bcf40a9358749b04a25825a4
PeiyuLiao <enzoliao95@gmail.com>
2017-02-11 19:12:35 +0800

    Update some comments

--- ch01/exercise3.scm
+++ ch01/exercise3.scm
@@ -1,3 +1,9 @@
+;; The solution of exercise 1.3
+;; Define a procedure that takes three numbers as arguments and returns
+;; the sum of the two larger numbers
+
+;; Input three numbers from keyboard
+;; Return the sum of the two larger numbers
 (define (getBiggerSum a b c)
   (if (> a b)
       (if (> b c)
@@ -11,7 +17,8 @@
   (display "Input three numbers:")
   (newline)
   (let ((a (read)) (b (read)) (c (read)))
-  (display (getBiggerSum a b c))
-  (newline)))
+    (display (getBiggerSum a b c))
+    (newline)))
 
 (main)
+
