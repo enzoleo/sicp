10c3082491f9c7317188c646f99e0e3f6da526db
PeiyuLiao <enzoliao95@gmail.com>
2017-02-27 15:08:00 +0800

    Add problem description

--- chapter_01/exercise_27.scm
+++ chapter_01/exercise_27.scm
@@ -1,4 +1,8 @@
 ;; The solution of exercise 1.27
+;; Demonstrate that the Carmichael numbers listed in footnote 47 really do
+;; fool the Fermat test. That is, write a procedure that takes an integer
+;; n and tests whether a ^ n is congruent to a modulo n for every a < n,
+;; and try your procedure on the given Carmichael numbers.
 ;;
 ;; -------- (above from SICP)
 ;;
