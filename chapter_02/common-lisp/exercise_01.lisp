9706b9ae4c4d89bff115aa2b84565df1bad120e2
PeiyuLiao <enzoliao95@gmail.com>
2017-03-16 20:58:36 +0800

    Update exercise 2.01

--- chapter_02/common-lisp/exercise_01.lisp
+++ chapter_02/common-lisp/exercise_01.lisp
@@ -1,47 +1,70 @@
 ;; The solution of exercise 2.1
-;; Data-abstraction barriers in the rational-number package.
-;; This simple idea has many advantages. One advantage is that it makes
-;; programs much easier to maintain and to modify. Any complex data
-;; structure can be represented in a variety of ways with the primitive
-;; data structures provided by a programming language. Of course, the
-;; choice of representation influences the programs that operate on it;
-;; thus, if the representation were to be changed at some later time, all
-;; such programs might have to be modified accordingly. This task could be
-;; time-consuming and expensive in the case of large programs unless the
-;; dependence on the representation were to be confined by design to a
-;; very few program modules. For example, an alternate way to address the
-;; problem of reducing rational numbers to lowest terms is to perform the
-;; reduction whenever we access the parts of a rational number, rather
-;; than when we construct it. This leads to different constructor and
-;; selector procedures:
-;;
-;;     (defun make-rat (n d)
-;;       (cons n d))
-;;     (defun numer (x)
-;;       (let ((g (gcd (car x) (cdr x))))
-;;         (/ (car x) g)))
-;;     (defun denom (x)
-;;       (let ((g (gcd (car x) (cdr x))))
-;;         (/ (cdr x) g)))
-;;
-;; The difference between this implementation and the previous one lies in
-;; when we compute the `gcd`. If in our typical use of rational numbers we
-;; access the numerators and denominators of the same rational numbers
-;; many times, it would be preferable to compute the gcd when the rational
-;; numbers are constructed. If not, we may be better off waiting until
-;; access time to compute the gcd. In any case, when we change from one
-;; representation to the other, the procedures add-rat, sub-rat, and so on
-;; do not have to be modified at all. Constraining the dependence on the
-;; representation to a few interface procedures helps us design programs
-;; as well as modify them, because it allows us to maintain the flexibility
-;; to consider alternate implementations. To continue with our simple
-;; example, suppose we are designing a rational-number package and we can't
-;; decide initially whether to perform the gcd at construction time or at
-;; selection time. The data-abstraction methodology gives us a way to defer
-;; that decision without losing the ability to make progress on the rest of
-;; the system.
+;; Define a better version of make-rat that handles both positive and
+;; negative arguments. Make-rat should normalize the sign so that if the
+;; rational number is positive, both the numerator and denominator are
+;; positive, and if the rational number is negative, only the numerator is
+;; negative.
 ;;
 ;; -------- (above from SICP)
 ;;
+
+;; Make a rational number with numerator and denominator
+(defun make-rat (n d)
+  (let* ((g (gcd n d))
+         (numerator (/ n g))
+         (denominator (/ d g)))
+    (if (< denominator 0)
+        (cons (- numerator) (- denominator))
+        (cons numerator denominator))))
+
+;;
+;; Note that we have another choice to define such procedures:
+;;
+;;     (defvar numer #'car)
+;;     (defvar denom #'cdr)
+;;
+;; The first definition associates the name make-rat with the value of the
+;; expression cons, which is the primitive procedure that constructs pairs.
+;; Thus make-rat and cons are names for the same primitive constructor.
+;; Defining selectors and constructors in this way is efficient: Instead of
+;; `make-rat` calling cons, make-rat is cons, so there is only one
+;; procedure called, not two, when make-rat is called. On the other hand,
+;; doing this defeats debugging aids that trace procedure calls or put
+;; breakpoints on procedure calls: You may want to watch make-rat being
+;; called, but you certainly don't want to watch every call to cons.
+;; (SICP)
+;;
+(defun numer (x) (car x))
+(defun denom (x) (cdr x))
+
+;; Simple operations
+(defun add-rat (x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(defun sub-rat (x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(defun mul-rat (x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(defun div-rat (x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(defun equal-rat? (x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+;;
+;; Actually using the procedure defined above is available. Because the
+;; result of function `gcd` has the same sign with the second argument,
+;; which means the denominator of the result of `make-rat` is always
+;; positive.
+
+(defun main ()
+  (format t "Load this file and use R - rationals ~%"))
+
+(main)
 
 
