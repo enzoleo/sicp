4a24e0819ff97f246b2426f959a481f8c46d0d8c
PeiyuLiao <enzoliao95@gmail.com>
2017-04-09 23:28:23 +0800

    Add scheme version for exercise 2.13 and update exercise 2.12

--- chapter_02/scheme/exercise_13.scm
+++ chapter_02/scheme/exercise_13.scm
@@ -1,29 +1,8 @@
-;; After debugging her program, Alyssa shows it to a potential user, who
-;; complains that her program solves the wrong problem. He wants a program
-;; that can deal with numbers represented as a center value and an
-;; additive tolerance; for example, he wants to work with intervals such
-;; as `3.5 ± 0.15` rather than [3.35, 3.65]. Alyssa returns to her desk
-;; and fixes this problem by supplying an alternate constructor and
-;; alternate selectors:
-;;
-;;     (define (make-center-width c w)
-;;       (make-interval (- c w) (+ c w)))
-;;     (define (center i)
-;;       (/ (+ (lower-bound i) (upper-bound i)) 2))
-;;     (define (width i)
-;;       (/ (- (upper-bound i) (lower-bound i)) 2))
-;;
-;; Unfortunately, most of Alyssa's users are engineers. Real engineering
-;; situations usually involve measurements with only a small uncertainty,
-;; measured as the ratio of the width of the interval to the midpoint of
-;; the interval. Engineers usually specify percentage tolerances on the
-;; parameters of devices, as in the resistor specifications given earlier.
-;;
-;; The solution of exercise 2.12
-;; Define a constructor make-center-percent that takes a center and a
-;; percentage tolerance and produces the desired interval. You must also
-;; define a selector percent that produces the percentage tolerance for a
-;; given interval. The center selector is the same as the one shown above.
+;; The solution of exercise 2.13
+;; Show that under the assumption of small percentage tolerances there is
+;; a simple formula for the approximate percentage tolerance of the
+;; product of two intervals in terms of the tolerances of the factors. You
+;; may simplify the problem by assuming that all numbers are positive.
 ;;
 ;; -------- (above from SICP)
 ;;
@@ -101,29 +80,15 @@
                                  (max (* lb-x lb-y)
                                       (* ub-x ub-y)))))))))
 
+;; The new multiplication procedure is defined here, under the assumption
+;; of small percentage tolerances .
 (define (mul-interval-cp x y)
   (let ((ctr-ix (center x))
         (pct-ix (percent x))
         (ctr-iy (center y))
         (pct-iy (percent y)))
-    (cond ((or (and (>= pct-iy 100) (< pct-ix 100))
-               (and (>= pct-ix 100) (> pct-iy pct-ix)))
-           (make-center-percent (* (+ 1 (/ pct-ix 100.0))
-                                   ctr-ix ctr-iy)
-                                pct-iy))
-          ((and (< pct-ix 100)
-                (< pct-iy 100))
-           (make-center-percent (* (+ 1 (/ (* pct-ix pct-iy)
-                                           10000.0))
-                                   ctr-ix ctr-iy)
-                                (/ (* (+ pct-ix pct-iy)
-                                      10000.0)
-                                   (+ (* pct-ix
-                                         pct-iy) 10000))))
-          (else
-           (make-center-percent (* (+ 1 (/ pct-iy 100.0))
-                                   ctr-ix ctr-iy)
-                                pct-ix)))))
+    (make-center-percent (* ctr-ix ctr-iy)
+                         (+ pct-ix pct-iy))))
 
 (define (div-interval x y)
   (let ((ub-y (upper-bound y))
@@ -176,45 +141,45 @@
 ;;
 ;; [GUILE]
 ;;     scheme@(guile-user)> (mul-interval-test-cp)
-;;     PRINT INTERVAL: [10.000499999999999, 29.99925] 
-;;     PRINT INTERVAL: 19.999875 (± 49.997187482421765%) 
-;;     PRINT INTERVAL: [-15.0, 30.0] 
-;;     PRINT INTERVAL: 7.5 (± 300.0%) 
-;;     PRINT INTERVAL: [-30.000300000000003, -1.9998000000000005] 
-;;     PRINT INTERVAL: -16.00005 (± 87.50128905847168%) 
-;;     PRINT INTERVAL: [-19.9995, 29.99925] 
-;;     PRINT INTERVAL: 4.999874999999999 (± 500.0000000000001%) 
-;;     PRINT INTERVAL: [-20.0, 30.0] 
-;;     PRINT INTERVAL: 5.0 (± 500.0%) 
-;;     PRINT INTERVAL: [-21.000149999999994, 30.000149999999994] 
-;;     PRINT INTERVAL: 4.5 (± 566.6699999999998%) 
-;;     PRINT INTERVAL: [-20.00025, 10.000125000000002] 
-;;     PRINT INTERVAL: -5.000062499999999 (± 300.0000000000001%) 
-;;     PRINT INTERVAL: [-25.0, 50.0] 
-;;     PRINT INTERVAL: 12.5 (± 300.0%) 
-;;     PRINT INTERVAL: [-9.0, 6.0] 
-;;     PRINT INTERVAL: -1.5 (± 500.0%) 
-;;     PRINT INTERVAL: [-5.9998499999999995, -2.0000999999999998] 
-;;     PRINT INTERVAL: -3.9999749999999996 (± 49.997187482421765%) 
-;;     PRINT INTERVAL: [-6.0, 3.0] 
-;;     PRINT INTERVAL: -1.5 (± 300.0%) 
-;;     PRINT INTERVAL: [2.0, 12.000000000000002] 
-;;     PRINT INTERVAL: 7.000000000000001 (± 71.42857142857143%) 
+;;     PRINT INTERVAL: [1865.625, 1884.375] 
+;;     PRINT INTERVAL: 1875.0 (± 0.5%) 
+;;     PRINT INTERVAL: [621.875, 628.125] 
+;;     PRINT INTERVAL: 625.0 (± 0.5%) 
+;;     PRINT INTERVAL: [-1380.5, -1369.5] 
+;;     PRINT INTERVAL: -1375.0 (± 0.4%) 
+;;     PRINT INTERVAL: [4092.0000000000005, 4158.0] 
+;;     PRINT INTERVAL: 4125.0 (± 0.7999999999999945%) 
+;;     PRINT INTERVAL: [1364.0, 1386.0] 
+;;     PRINT INTERVAL: 1375.0 (± 0.8%) 
+;;     PRINT INTERVAL: [4628.25, 4721.75] 
+;;     PRINT INTERVAL: 4675.0 (± 1.0%) 
+;;     PRINT INTERVAL: [-1883.625, -1866.375] 
+;;     PRINT INTERVAL: -1875.0 (± 0.45999999999999996%) 
+;;     PRINT INTERVAL: [1493.25, 1506.75] 
+;;     PRINT INTERVAL: 1500.0 (± 0.44999999999999996%) 
+;;     PRINT INTERVAL: [-1666.4999999999998, -1633.5000000000002] 
+;;     PRINT INTERVAL: -1650.0 (± 0.9999999999999861%) 
+;;     PRINT INTERVAL: [-1884.375, -1865.625] 
+;;     PRINT INTERVAL: -1875.0 (± 0.5%) 
+;;     PRINT INTERVAL: [-1381.875, -1368.125] 
+;;     PRINT INTERVAL: -1375.0 (± 0.5%) 
+;;     PRINT INTERVAL: [622.5, 627.5] 
+;;     PRINT INTERVAL: 625.0 (± 0.4%)
 ;;
 
 (define (mul-interval-test-cp)
-  (test-mul-cp 2.5 20 7.5 33.33)
-  (test-mul-cp 2.5 20 2.5 300)
-  (test-mul-cp 2.5 20 -5.5 81.82)
-  (test-mul-cp 0.5 500 7.5 33.33)
-  (test-mul-cp 0.5 500 2.5 300)
-  (test-mul-cp 0.5 500 1.5 566.67)
-  (test-mul-cp -0.75 166.67 2.5 300)
-  (test-mul-cp -2.5 300 -2 150)
-  (test-mul-cp 0.5 500 -2 50)
-  (test-mul-cp -2.5 20 1.5 33.33)
-  (test-mul-cp -2.5 20 0.5 300)
-  (test-mul-cp -2.5 20 -2.5 60))
+  (test-mul-cp 25 0.2 75 0.3)
+  (test-mul-cp 25 0.2 25 0.3)
+  (test-mul-cp 25 0.2 -55 0.2)
+  (test-mul-cp 55 0.5 75 0.3)
+  (test-mul-cp 55 0.5 25 0.3)
+  (test-mul-cp 55 0.5 85 0.5)
+  (test-mul-cp -75 0.16 25 0.3)
+  (test-mul-cp -25 0.3 -60 0.15)
+  (test-mul-cp 55 0.5 -30 0.5)
+  (test-mul-cp -25 0.2 75 0.3)
+  (test-mul-cp -25 0.2 55 0.3)
+  (test-mul-cp -25 0.2 -25 0.2))
 
 (define (main)
   (display "Load this file and use `make-interval`.\n")
