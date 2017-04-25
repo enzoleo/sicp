207f2a1380e5da843a4a9bbfda50429b19987f9a
PeiyuLiao <enzoliao95@gmail.com>
2017-04-25 22:17:24 +0800

    Update exercise 2.7 to 2.11 (python)

--- chapter_02/python/sicpc2e11.py
+++ chapter_02/python/sicpc2e11.py
@@ -8,6 +8,7 @@
 ##
 
 import sys
+import math
 
 ## Check the python version
 ## Write different code for different python version
@@ -15,142 +16,190 @@ if sys.version_info[0] < 3:
     # Use new type class
     __metaclass__ = type
 
-##
+    # Define functions to detect nan and infty value
+    def isnan(a):
+        """Check whether a is nan."""
+        return a != a
+
+    def isinf(a):
+        """Check whether a is infty."""
+        infty = float('inf')
+        return (a == infty or a == -infty)
+
+else:
+    isnan = math.isnan
+    isinf = math.isinf
+
 ## Define the interval class.
-## Here we define a interval class with 2 attributes: lower-bound and
-## upper-bound. To construct the instance of this class, the 2 arguments
-## (@lower_bound and @upper_bound) are needed.
-##
 class Interval:
+    """Here we define a interval class with 2 attributes: lower-bound and
+    upper-bound. To construct the instance of this class, the 2 arguments
+    (@lower_bound and @upper_bound) are needed.
 
-    # Infinity number only in class Interval
-    infty = float('inf')
+    Construct the interval with lower bound and upper bound directly. But
+    the constructor will raise an error if your lower bound is larger
+    than upper bound.    
+    """
 
+    __slots__ = ('__lb', '__ub')
+    
     # The initialization with two parameters: lower and upper bounds
     def __init__(self, lb, ub):
         """Initialize the interval class.
         You must supply the lower and upper bounds of this interval.
+        Add your lower_bound is not allowed to be larger than upper_bound.
         """
         # Notice that you can also import math module and use function
         # math.isnan() to check whether the lower bound or upper bound is
         # NaN. But here we do not use this method.
-        assert lb >= -self.infty and ub <= self.infty, \
-            "NaN value of bounds occurs!"
-        assert lb <= ub, "Lower bound is larger than upper bound!"
+        if isnan(lb) or isnan(ub):
+            raise ValueError("nan value of bounds occurs: %r, %r" % \
+                             (lb, ub))
+        if not (isinstance(lb, (int, float)) and \
+                isinstance(ub, (int, float))):
+            raise TypeError("cannot initialize interval with bounds "
+                            "in wrong type: %s, %s" % \
+                            (type(lb).__name__, type(ub).__name__))
+        if lb > ub:
+            raise ValueError("lower bound is larger than upper bound: "
+                             "%r > %r" % (lb, ub))
         
-        self.lower_bound = lb
-        self.upper_bound = ub
+        self.__lb, self.__ub = lb, ub
 
-    def get_bounds(self):
-        """Return the lower and upper bounds"""
-        return self.lower_bound, self.upper_bound
+    @classmethod
+    def from_number(self, number):
+        """Initialize the interval class from a number.
+        Notice that a number can be seen as an interval whose lower bound
+        equals to upper bound.
+        """
+        if not isinstance(number, (int, float)):
+            raise TypeError("cannot initialize interval with bounds "
+                            "in wrong type: %s" % type(number).__name__)
+        return Interval(number, number)
 
-    def set_bounds(self, bounds):
-        """Reset the center and percent"""
-        assert bounds[0] <= bounds[1], \
-            "Lower bound is larger than upper bound!"
-        self.lower_bound, self.upper_bound = bounds
+    @property
+    def lb(itv):
+        return itv.__lb
 
-    # Property function
-    bounds = property(get_bounds, set_bounds)
+    @lb.setter
+    def lb(itv, new_lb):
+        if new_lb > itv.__ub:
+            raise ValueError("lower bound is larger than upper bound: "
+                             "%r > %r" % (new_lb, itv.__ub))
+        itv.__lb = new_lb
+    
+    @property
+    def ub(itv):
+        return itv.__ub
+
+    @ub.setter
+    def ub(itv, new_ub):
+        if new_ub < itv.__lb:
+            raise ValueError("lower bound is larger than upper bound: "
+                             "%r > %r" % (itv.__lb, new_ub))
+        itv.__ub = new_ub
+
+    @property
+    def bounds(itv):
+        return itv.__lb, itv.__ub
+
+    @bounds.setter
+    def bounds(itv, new_bounds):
+        if new_bounds[0] < new_bounds[1]:
+            raise ValueError("lower bound is larger than upper bound: "
+                             "%r > %r" % (new_bounds[0], new_bounds[1]))
+        itv.__lb, itv.__ub = new_bounds
 
     def __repr__(self):
         """Print method"""
         # Print all attributes of this interval
-        ostr = "Interval \t[%s, %s]" % \
-               (self.lower_bound, self.upper_bound)
-        return ostr
+        return 'Interval \t[%s, %s]' % \
+               (self.__lb, self.__ub)
             
     def __eq__(self, itv):
         """Equality determination"""
         # Only two intervals with completely the same attributes are
         # equal. The attributes of an interval are determined by the two
         # independent attributes named @lower_bound and @upper_bound.
-        return (self.lower_bound == itv.lower_bound and \
-                self.upper_bound == itv.upper_bound)
+        if isinstance(itv, Interval):
+            return (self.lb == itv.lb and self.ub == itv.ub)
+        else:
+            return False
         
-    def __add__(self, itv):
+    def __add__(self, addend):
         """Interval Addition"""
-        infty = self.infty
+        if isinstance(addend, Interval):
+            return Interval(self.lb + addend.lb,
+                            self.ub + addend.ub)
+        elif isinstance(addend, (int, float)):
+            return Interval(self.lb + addend,
+                            self.ub + addend)
+        return NotImplemented
 
-        new_lb = self.lower_bound + itv.lower_bound
-        new_ub = self.upper_bound + itv.upper_bound
-
-        # The value of lower_bound and upper_bound could not be nan,
-        # which means they must satisfy the following inequalities.
-        assert -infty <= new_lb and new_ub <= infty, \
-            "NaN value of bounds occurs!"
-        
-        return Interval(new_lb, new_ub)
-
-    def __sub__(self, itv):
-        """Interval subtraction"""
-        infty = self.infty
-        
-        new_lb = self.lower_bound - itv.upper_bound
-        new_ub = self.upper_bound - itv.lower_bound
-
-        # The value of lower_bound and upper_bound could not be nan,
-        # which means they must satisfy the following inequalities.
-        assert -infty <= new_lb and new_ub <= infty, \
-            "NaN value of bounds occurs!"
-        
-        return Interval(new_lb, new_ub)
+    def __sub__(self, minuend):
+        """Interval Subtraction"""
+        if isinstance(minuend, Interval):
+            return Interval(self.lb - minuend.ub,
+                            self.ub - minuend.lb)
+        elif isinstance(minuend, (int, float)):
+            return Interval(self.lb - minuend,
+                            self.ub - minuend)
+        return NotImplemented
     
-    def __mul__(self, itv):
+    def __mul__(self, multiplier):
         """Interval multiplication"""
-        lb_x  = self.lower_bound
-        ub_x  = self.upper_bound
-        lb_y  =  itv.lower_bound
-        ub_y  =  itv.upper_bound
+        if isinstance(multiplier, Interval):
+            lb_x = self.lb
+            ub_x = self.ub
+            lb_y = multiplier.lb 
+            ub_y = multiplier.ub
 
-        # Conditions to do multiplication
-        if lb_x >= 0:
-            if lb_y >= 0:
-                return Interval(lb_x * lb_y, ub_x * ub_y)
-            elif ub_y < 0:
-                return Interval(ub_x * lb_y, lb_x * ub_y)
-            else:
-                return Interval(ub_x * lb_y, ub_x * ub_y)
-        elif ub_x < 0:
-            if lb_y >= 0:
+            # Conditions to do multiplication
+            if lb_x >= 0:
+                if lb_y >= 0:
+                    return Interval(lb_x * lb_y, ub_x * ub_y)
+                elif ub_y < 0:
+                    return Interval(ub_x * lb_y, lb_x * ub_y)
+                else:
+                    return Interval(ub_x * lb_y, ub_x * ub_y)
+            elif ub_x < 0:
+                if lb_y >= 0:
                 return Interval(lb_x * ub_y, ub_x * lb_y)
-            elif ub_y < 0:
-                return Interval(ub_x * ub_y, lb_x * lb_y)
+                elif ub_y < 0:
+                    return Interval(ub_x * ub_y, lb_x * lb_y)
+                else:
+                    return Interval(lb_x * ub_y, lb_x * lb_y)
             else:
-                return Interval(lb_x * ub_y, lb_x * lb_y)
-        else:
-            if lb_y >= 0:
-                return Interval(lb_x * ub_y, ub_x * ub_y)
-            elif ub_y < 0:
-                return Interval(ub_x * lb_y, lb_x * lb_y)
+                if lb_y >= 0:
+                    return Interval(lb_x * ub_y, ub_x * ub_y)
+                elif ub_y < 0:
+                    return Interval(ub_x * lb_y, lb_x * lb_y)
+                else:
+                    return Interval(min(ub_x * lb_y, lb_x * ub_y), \
+                                    max(lb_x * lb_y, ub_x * ub_y))    
+        elif isinstance(multiplier, (int, float)):
+            if multiplier >= 0:
+                return Interval(self.lb * multiplier,
+                                self.ub * multiplier)
             else:
-                return Interval(min(ub_x * lb_y, lb_x * ub_y), \
-                                max(lb_x * lb_y, ub_x * ub_y))
-        return Interval(0, 0)
+                return Interval(self.ub * multiplier,
+                                self.lb * multiplier)
+        return NotImplemented
 
-    def rdiv_interval(self, num):
+    def _rdiv(self, other):
         """An interval divides a real number"""
-        # Notice that zero cannot be denominator
-        assert not self.lower_bound <= 0 <= self.upper_bound, \
-            "The dividend interval spans zero!"
+        if isinstance(other, (int, float)):
+            if self.lb <= 0 <= self.ub:
+                raise ValueError("the dividend interval spans zero")
+            if isinf(other):
+                return Interval(other, other)
 
-        if num == self.infty or num == -self.infty:
-            return Interval(num, num)
-        
-        if num > 0:
-            return Interval(num * 1.0 / self.upper_bound, \
-                            num * 1.0 / self.lower_bound)
-        else:
-            return Interval(num * 1.0 / self.lower_bound, \
-                            num * 1.0 / self.upper_bound)
-        return Interval(0, 0)
+            itv = Interval(1.0 / self.ub, 1.0 / self.lb)
+            return itv.__mul__(other)
+        return NotImplemented
 
-    def div_interval(self, itv):
+    def _div(self, itv):
         """An interval divides another interval"""
-        # Notice that zero cannot be denominator
-        assert not itv.lower_bound <= 0 <= itv.upper_bound, \
-            "The dividend interval spans zero!"
         return self.__mul__(1 / itv)
         
     # Here we check the current python version. If the current Python
@@ -164,11 +213,11 @@ class Interval:
     # In Python 3, all numeric division with operator `/` results in a
     # float result while it does not do that in Python 2.
     if sys.version_info[0] >= 3:
-        __rtruediv__ = rdiv_interval
-        __truediv__  =  div_interval
+        __rtruediv__ = _rdiv
+        __truediv__  =  _div
     else:
-        __rdiv__ = rdiv_interval
-        __div__  =  div_interval
+        __rdiv__ = _rdiv
+        __div__  =  _div
     
 
 
