87c8ea0fffeceadf05681d25ae6db43d2f4ef037
PeiyuLiao <enzoliao95@gmail.com>
2017-05-08 18:02:08 +0800

    Add exercise 1.25 (haskell)

---
+++ chapter_01/haskell/sicpc1e25.hs
@@ -0,0 +1,149 @@
+-- The solution of exercise 1.25
+-- Alyssa P. Hacker complains that we went to a lot of extra work in
+-- writing `expmod`. After all, she says, since we already know how to
+-- compute exponentials, we could have simply written
+--
+--     (define (expmod base exp m)
+--       (modulo (fast-expt base exp) m))
+--
+-- Is she correct? Would this procedure serve as well for our fast prime
+-- tester? Explain.
+--
+-- -------- (above from SICP)
+--
+import Data.Time
+import System.Random
+-- Run 'cabal install vector' first!
+import qualified Data.Vector as V
+
+-- Square function
+square x = x * x
+
+-- Fast exponential function
+fastExpt :: (Integral a, Integral b) => a -> b -> a
+fastExpt x n =
+  let iter a base 0 = a
+      iter a base n =
+        case (mod n 2) of 1 -> iter (a * base) base (n - 1)
+                          0 -> iter a (base * base) (n `div` 2)
+  in iter 1 x n
+
+-- To implement the Fermat test, we need a procedure that computes the
+-- exponential of a number modulo another number
+expmod :: (Integral a) => a -> a -> a -> a
+expmod base exp m =
+  mod (fastExpt base exp) m
+
+--
+-- It is not easy to generate random numbers. Usually random numbers can
+-- be generated easily in IO action but here we prefer the methods much
+-- more purely. The following `fastPrime` function chooses a random number
+-- between 1 and n - 1 and check if it satisfies `expmod base n n == base`
+-- for @times times, here an init generator needed, such as (mkStdGen 0).
+--
+-- Choose a random number between 1 and n - 1 using the procedure `random`,
+-- which we assume is included as a primitive in Haskell. The following
+-- procedure runs the test a given number of times, as specified by a
+-- parameter. Its value is true if the test succeeds every time, and false
+-- otherwise.
+--
+fastPrime :: (Integral a, Random a, RandomGen g) => a -> a -> g -> Bool
+fastPrime n 0 initGen = True
+fastPrime n times initGen =
+  let (randNum, nextGen) = randomR (1, n - 1) initGen
+  in if expmod randNum n n == randNum
+     then fastPrime n (times - 1) nextGen
+     else False
+
+-- Test a number is a prime or not. Here we do fermat-test 20 times for
+-- each number n. You can modify the number as you like.
+isPrime n =
+  fastPrime n 20 $ mkStdGen 0
+
+-- Find the smallest prime that is larger than n
+nextPrime :: (Integral a, Random a) => a -> a
+nextPrime n =
+  let findPrime counter =
+        if isPrime counter
+        then counter
+        else findPrime (counter + 1)
+  in findPrime (n + 1)
+
+-- Find the smallest m primes that are larger than n
+nextPrimes :: (Integral a, Show a, Random a) => a -> a -> IO ()
+nextPrimes n m =
+  let searchPrimeCount init counter maxCount =
+        if counter < maxCount
+        then do let newPrime = nextPrime init
+                putStrLn ("prime[" ++ show counter ++ "] "
+                          ++ show newPrime)
+                searchPrimeCount newPrime (counter + 1) maxCount
+        else return ()
+  in searchPrimeCount n 0 m
+
+-- Find the m th primes that are larger than n
+nextPrimesNum :: (Integral a, Random a) => a -> a -> a
+nextPrimesNum n m =
+  let searchPrimeCount init counter maxCount =
+        if counter < maxCount
+        then do let newPrime = nextPrime init
+                searchPrimeCount newPrime (counter + 1) maxCount
+        else init
+  in searchPrimeCount n 0 m
+
+--
+-- Find the smallest m primes that are bigger than n and store all of them
+-- in a vector. Test an example: find the next 100 odd primes of 19999.
+--
+-- *Main> nextPrimesVec 19999 100
+-- [20011,20021,20023,20029,20047,20051,20063,20071,20089,20101,20107,
+--  20113,20117,20123,20129,20143,20147,20149,20161,20173,20177,20183,
+--  20201,20219,20231,20233,20249,20261,20269,20287,20297,20323,20327,
+--  20333,20341,20347,20353,20357,20359,20369,20389,20393,20399,20407,
+--  20411,20431,20441,20443,20477,20479,20483,20507,20509,20521,20533,
+--  20543,20549,20551,20563,20593,20599,20611,20627,20639,20641,20663,
+--  20681,20693,20707,20717,20719,20731,20743,20747,20749,20753,20759,
+--  20771,20773,20789,20807,20809,20849,20857,20873,20879,20887,20897,
+--  20899,20903,20921,20929,20939,20947,20959,20963,20981,20983,21001,
+--  21011]
+--
+nextPrimesVec :: (Integral a, Random a) => a -> a -> V.Vector a
+nextPrimesVec n m =
+  let searchPrimeCount init counter maxCount vector =
+        if counter < maxCount
+        then let newPrime = nextPrime init
+             in searchPrimeCount newPrime
+                                 (counter + 1)
+                                 maxCount
+                                 (V.snoc vector newPrime)
+        else vector
+      v0 = V.fromList []
+  in searchPrimeCount n 0 m v0
+
+-- Compute the runtime of `nextPrimesNum`
+computeRuntime n m = do
+  start <- getCurrentTime
+  print $ nextPrimesNum n m
+  stop <- getCurrentTime
+  print $ diffUTCTime stop start
+
+--
+-- The method by Alyssa is actually available. But note that for some big
+-- numbers (such as 1,000,000), The efficiency is very very low. Because
+-- it takes a lot of time to compute the result of expression a ^ n (n is
+-- a huge number). Perhaps the result is so big that it causes stack
+-- overflow.
+--
+-- *Main> computeRuntime 10000 3
+-- 10037
+-- 0.055042s
+-- *Main> computeRuntime 100000 3
+-- 100043
+-- 0.739085s
+-- *Main> computeRuntime 1000000 3
+-- 1000037
+-- 15.399934s
+--
+
+
+
