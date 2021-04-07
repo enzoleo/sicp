b38e7aa92b1640cf758a59d94f6bc058d7bf97aa
Ai Miyano <enzoliao95@gmail.com>
2021-04-07 12:26:48 +0800

    Fix wrong specifications of Chapter1 Exercise 1.27

--- ch1/sicpc1e27.hs
+++ ch1/sicpc1e27.hs
@@ -1,10 +1,8 @@
--- The solution of exercise 1.24
--- Modify the `timed-prime-test` procedure of exercise 1.22 to use
--- `fast-prime?` (the Fermat method), and test each of the 12 primes you
--- found in that exercise. Since the Fermat test has O(log n) growth, how
--- would you expect the time to test primes near 1,000,000 to compare with
--- the time needed to test primes near 1000? Do your data bear this out?
--- Can you explain any discrepancy you find?
+-- The solution of exercise 1.27
+-- Demonstrate that the Carmichael numbers listed in Footnote 1.47 really
+-- do fool the Fermat test. That is, write a procedure that takes an
+-- integer n and tests whether an is congruent to a modulo n for every
+-- a < n, and try your procedure on the given Carmichael numbers.
 --
 -- -------- (above from SICP)
 --
